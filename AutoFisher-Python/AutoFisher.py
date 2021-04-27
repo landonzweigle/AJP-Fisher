e71e758d5090b7e95b4ababe58bc181f1ae9b03c
Landon Zweigle <lzweigle@rams.colostate.edu>
2021-04-27 14:04:24 -0600

    Moved JPComms to its own file

--- AutoFisher-Python/AutoFisher.py
+++ AutoFisher-Python/AutoFisher.py
@@ -1,5 +1,5 @@
-import socket, RLNN, math, time
-from typing import MappingView
+import RLNN, math, sys
+import JavaPythonComms as JPComms
 
 from numpy.core.defchararray import split
 import numpy as np, pandas as pds
@@ -53,94 +53,6 @@ n_inputs = 3 #{deltaP, deltaV, action}
 DQN = RLNN.RLNeuralNetwork(validActions, Epsilon, n_inputs, [100], 1)
 DQN.createStandards(Xmeans, Xstds, Tmean, Tstd)
 ####################
-
-
-
-
-
-class JPComms:
-    CNN_ATMPTS = 10
-    sock = None    
-    framemode = None
-    gamemode = None
-
-
-    def __init__(self, mode):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-        self.framemode, self.gamemode = mode
-        
-        trysLeft = self.CNN_ATMPTS
-        connected = False
-        lastExep = None
-        while(trysLeft > 0 and not connected):
-            try:
-                self.sock.connect(("localhost",13337))
-                connected = True
-            except Exception as e:
-                lastExep = e
-                trysLeft -= 1
-
-                if(trysLeft > 0):
-                    time.sleep(0.25)
-                else:
-                    print("Ran out of connection attempts")
-                    raise lastExep
-
-
-        self.startupSeq()
-
-
-
-    def startupSeq(self):
-        firstMsg = self.recvStr()
-        print("FM: " + firstMsg)
-        if(firstMsg=="Hello Python :)"):
-            self.sendStr("Hello Java :D")
-            #recv expected framemode and gamemode:
-            print("Getting modes")
-            actFrameMode = self.recvStr()
-            actGameMode = self.recvStr()
-            print("modes: " + actFrameMode + " " + actGameMode)
-            print("Checking modes")
-            if ( (actFrameMode != self.framemode) or (actGameMode != self.gamemode) ):
-                self.sendStr("MMM")
-                raise Exception("modes dont match")
-            self.sendStr("")
-            print("\"HandShake\" successful")
-            return
-            
-    def sendInt(self, toSend):
-        self.sock.sendall(toSend.to_bytes(4, 'big'))
-            
-    def recvInt(self):
-        intBytes = self.sock.recv(4)
-        return int.from_bytes(intBytes, 'big')
-
-    def sendStr(self, toSend):
-        if(type(toSend)==str):
-
-            enc = toSend.encode('utf-8')
-            msgLen = len(enc)
-            self.sock.sendall(msgLen.to_bytes(4, 'big')) #send message length
-            self.sock.sendall(enc) #send Message.
-        
-
-    def recvStr(self):
-        if(not self.sock):
-            return -1
-        msgLen = int.from_bytes(self.sock.recv(4),'big')
-
-        msg = self.sock.recv(msgLen)
-        return msg.decode('utf-8')
-        
-
-
-
-
-
-
-
 
 def runFrameByFrame(JPC):
     FramesToPlay = framesPerTrial * nTrials
@@ -245,7 +157,7 @@ def runFrameByFrame(JPC):
 
 
 def main():
-    JPC = JPComms(modesExcpected["TRAIN"])
+    JPC = JPComms.JPComms(modesExcpected["TRAIN"])
     print("\n-----------------------------------------------------")
     r, rLast2 = runFrameByFrame(JPC)
 
---
+++ AutoFisher-Python/JavaPythonComms.py
@@ -0,0 +1,77 @@
+import socket, time
+
+class JPComms:
+    CNN_ATMPTS = 10
+    sock = None    
+    framemode = None
+    gamemode = None
+
+
+    def __init__(self, mode):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        self.framemode, self.gamemode = mode
+        
+        trysLeft = self.CNN_ATMPTS
+        connected = False
+        lastExep = None
+        while(trysLeft > 0 and not connected):
+            try:
+                self.sock.connect(("localhost",13337))
+                connected = True
+            except Exception as e:
+                lastExep = e
+                trysLeft -= 1
+
+                if(trysLeft > 0):
+                    time.sleep(0.25)
+                else:
+                    print("Ran out of connection attempts")
+                    raise lastExep
+
+
+        self.startupSeq()
+
+
+
+    def startupSeq(self):
+        firstMsg = self.recvStr()
+        print("FM: " + firstMsg)
+        if(firstMsg=="Hello Python :)"):
+            self.sendStr("Hello Java :D")
+            #recv expected framemode and gamemode:
+            print("Getting modes")
+            actFrameMode = self.recvStr()
+            actGameMode = self.recvStr()
+            print("modes: " + actFrameMode + " " + actGameMode)
+            print("Checking modes")
+            if ( (actFrameMode != self.framemode) or (actGameMode != self.gamemode) ):
+                self.sendStr("MMM")
+                raise Exception("modes dont match")
+            self.sendStr("")
+            print("\"HandShake\" successful")
+            return
+            
+    def sendInt(self, toSend):
+        self.sock.sendall(toSend.to_bytes(4, 'big'))
+            
+    def recvInt(self):
+        intBytes = self.sock.recv(4)
+        return int.from_bytes(intBytes, 'big')
+
+    def sendStr(self, toSend):
+        if(type(toSend)==str):
+
+            enc = toSend.encode('utf-8')
+            msgLen = len(enc)
+            self.sock.sendall(msgLen.to_bytes(4, 'big')) #send message length
+            self.sock.sendall(enc) #send Message.
+        
+
+    def recvStr(self):
+        if(not self.sock):
+            return -1
+        msgLen = int.from_bytes(self.sock.recv(4),'big')
+
+        msg = self.sock.recv(msgLen)
+        return msg.decode('utf-8')
