02232afb166dc616e05c6f94aaf854a3cb11483d
Keegan Kochis <keeganjkochis@gmail.com>
2021-04-30 21:34:02 -0600

    Actually fix tab v space

--- createExperiments.py
+++ createExperiments.py
@@ -56,39 +56,39 @@ def main(runnerName):
 	# fpt vals = [5, 15, 35]
 	# hidden layer archs = [ [20], [50], [100], [50,50], [30,80], [80,30], [40,40,40], [20,80,20], [80,20,80] ]
 	
-	if runnerName == "landon":
-		experiments.addExperiment(5, 10000, [20], 100, 0.01, 0.8)
-		experiments.addExperiment(5, 40000, [100], 100, 0.01, 0.8)
-		experiments.addExperiment(5, 20000, [40,40,40], 100, 0.01, 0.8)
-		experiments.addExperiment(5, 10000, [30,80], 100, 0.01, 0.8)
-		experiments.addExperiment(5, 40000, [50,50], 200, 0.01, 0.8)
-		
-		experiments.addExperiment(10, 10000, [30,80], 200, 0.01, 0.8)
-		experiments.addExperiment(10, 5000, [100], 200, 0.01, 0.8)
-		experiments.addExperiment(10, 20000, [50], 200, 0.01, 0.8)
-		experiments.addExperiment(10, 10000, [80,20,80], 300, 0.01, 0.8)
-		
-		experiments.addExperiment(30, 1667, [40,40,40], 300, 0.01, 0.8)
-		experiments.addExperiment(30, 6666, [80,20,80], 300, 0.01, 0.8)
-		experiments.addExperiment(30, 3333, [50,50], 300, 0.01, 0.8)
-		experiments.addExperiment(30, 1667, [50], 100, 0.01, 0.8)
-		experiments.addExperiment(30, 6666, [20], 100, 0.01, 0.8)
-	elif runnerName == "keegan":
-		experiments.addExperiment(5, 20000, [50], 100, 0.01, 0.8)
-		experiments.addExperiment(5, 10000, [80,20,80], 100, 0.01, 0.8)
-		experiments.addExperiment(5, 40000, [20,80,20], 200, 0.01, 0.8)
-		experiments.addExperiment(5, 20000, [80,30], 200, 0.01, 0.8)
-		
-		experiments.addExperiment(10, 5000, [50,50], 200, 0.01, 0.8)
-		experiments.addExperiment(10, 20000, [80,30], 200, 0.01, 0.8)
-		experiments.addExperiment(10, 10000, [20], 300, 0.01, 0.8)
-		experiments.addExperiment(10, 5000, [20,80,20], 300, 0.01, 0.8)
-		experiments.addExperiment(10, 20000, [40,40,40], 300, 0.01, 0.8)
-		
-		experiments.addExperiment(30, 3333, [20,80,20], 300, 0.01, 0.8)
-		experiments.addExperiment(30, 1667, [80,30], 100, 0.01, 0.8)
-		experiments.addExperiment(30, 6666, [30,80], 200, 0.01, 0.8)
-		experiments.addExperiment(30, 3333, [100], 300, 0.01, 0.8)
+    if runnerName == "landon":
+	    experiments.addExperiment(5, 10000, [20], 100, 0.01, 0.8)
+	    experiments.addExperiment(5, 40000, [100], 100, 0.01, 0.8)
+	    experiments.addExperiment(5, 20000, [40,40,40], 100, 0.01, 0.8)
+	    experiments.addExperiment(5, 10000, [30,80], 100, 0.01, 0.8)
+	    experiments.addExperiment(5, 40000, [50,50], 200, 0.01, 0.8)
+	    
+	    experiments.addExperiment(10, 10000, [30,80], 200, 0.01, 0.8)
+	    experiments.addExperiment(10, 5000, [100], 200, 0.01, 0.8)
+	    experiments.addExperiment(10, 20000, [50], 200, 0.01, 0.8)
+	    experiments.addExperiment(10, 10000, [80,20,80], 300, 0.01, 0.8)
+	    
+	    experiments.addExperiment(30, 1667, [40,40,40], 300, 0.01, 0.8)
+	    experiments.addExperiment(30, 6666, [80,20,80], 300, 0.01, 0.8)
+	    experiments.addExperiment(30, 3333, [50,50], 300, 0.01, 0.8)
+	    experiments.addExperiment(30, 1667, [50], 100, 0.01, 0.8)
+	    experiments.addExperiment(30, 6666, [20], 100, 0.01, 0.8)
+    elif runnerName == "keegan":
+	    experiments.addExperiment(5, 20000, [50], 100, 0.01, 0.8)
+	    experiments.addExperiment(5, 10000, [80,20,80], 100, 0.01, 0.8)
+	    experiments.addExperiment(5, 40000, [20,80,20], 200, 0.01, 0.8)
+	    experiments.addExperiment(5, 20000, [80,30], 200, 0.01, 0.8)
+	    
+	    experiments.addExperiment(10, 5000, [50,50], 200, 0.01, 0.8)
+	    experiments.addExperiment(10, 20000, [80,30], 200, 0.01, 0.8)
+	    experiments.addExperiment(10, 10000, [20], 300, 0.01, 0.8)
+	    experiments.addExperiment(10, 5000, [20,80,20], 300, 0.01, 0.8)
+	    experiments.addExperiment(10, 20000, [40,40,40], 300, 0.01, 0.8)
+	    
+	    experiments.addExperiment(30, 3333, [20,80,20], 300, 0.01, 0.8)
+	    experiments.addExperiment(30, 1667, [80,30], 100, 0.01, 0.8)
+	    experiments.addExperiment(30, 6666, [30,80], 200, 0.01, 0.8)
+	    experiments.addExperiment(30, 3333, [100], 300, 0.01, 0.8)
 		
     csvPath.mkdir(parents=True, exist_ok=True) #make the dir if it doesnt exist.
     df = experiments.toDict()
